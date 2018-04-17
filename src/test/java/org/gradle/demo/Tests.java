9fa69c01aa8c3ecde820f34167d18a5a3473f82e
Markus Pikkanen <markus.pikkanen@gmail.com>
2018-04-17 11:05:05 +0300

    travis

---
+++ src/test/java/org/gradle/demo/Tests.java
@@ -0,0 +1,12 @@
+package org.gradle.demo;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class Tests {
+
+    @Test
+    public void dummyTest() {
+        assertEquals(0,1);
+    }
+}
